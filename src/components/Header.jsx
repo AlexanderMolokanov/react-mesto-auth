776a25a7252c4b0928fa67c44140b207d289f189
AlexanderMolokanov <91531787+AlexanderMolokanov@users.noreply.github.com>
2022-10-09 19:05:26 +0300

    пофиксить хедер

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ import logo from "../images/header_logo.svg";
 
 function Header({ onLogoutClick }) {
   const currentUser = useContext(CurrentUserContext);
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
 
   const toggleMenu = () => setMenuIsOpen(!menuIsOpen);
   // console.log(currentUser?.isLoggedIn);
 
   const logoutMobile = () => {
     onLogoutClick();
-    setMenuIsOpen(false);
+    setMenuIsOpen(true);
   };
 
   const Icons = () => (
@@ -28,32 +28,46 @@ function Header({ onLogoutClick }) {
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта" />
 
-      <div
-        className={
-          menuIsOpen
-            ? "header__burger-menu header__burger-menu_open"
-            : "header__burger-menu"
-        }
-      >
-        <span className="header__info header__info_mobile">
-          {currentUser?.email}
-        </span>
-
-        <Link
-          className="header__link header__link_mobile"
-          onClick={logoutMobile}
-          to="#"
-        >
-          ВыйтиB
-        </Link>
-      </div>
-
       <Switch>
-        <Route path="/">
+        <Route exact path="/sign-in">
           <>
+            <Link className="header__link" to="/sign-up">
+              Регистрация
+            </Link>
+          </>
+        </Route>
+
+        <Route exact path="/sign-up">
+          <>
+            <Link className="header__link" to="/sign-in">
+              Войти
+            </Link>
+          </>
+        </Route>
+        <Route exact path="/">
+          <>
+            <div
+              className={ menuIsOpen ? "header__burger-menu header__burger-menu_open" : "header__burger-menu"   }
+            >
+              <span className="header__info header__info_mobile">
+                {currentUser?.email}
+              </span>
+
+              <Link
+                className="header__link header__link_mobile"
+                onClick={logoutMobile}
+                to="#"
+              >
+                ВыйтиB
+              </Link>
+            </div>
+
             <div className="header__info-wrapper">
               <div className="header__info">
-                {currentUser?.email}
+                <span className="header__info header__info_mobile">
+                  {currentUser?.email}
+                </span>
+
                 <Link
                   className="header__link header__link_logout"
                   onClick={onLogoutClick}
@@ -64,22 +78,6 @@ function Header({ onLogoutClick }) {
               </div>
             </div>
             <Icons />
-          </>
-        </Route>
-
-        <Route path="/sign-in">
-          <>
-            <Link className="header__link" to="/sign-up">
-              Регистрация
-            </Link>
-          </>
-        </Route>
-
-        <Route path="/sign-up">
-          <>
-            <Link className="header__link" to="/sign-in">
-              Войти
-            </Link>
           </>
         </Route>
       </Switch>
